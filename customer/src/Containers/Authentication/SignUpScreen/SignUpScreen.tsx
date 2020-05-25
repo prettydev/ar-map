3d7146cba28f26cfd69cb5c4058aa7136e60e98e
shinystar <creditcoder@hotmail.com>
2020-05-24 23:49:51 -0400

    fix image contain->cover

--- customer/src/Containers/Authentication/SignUpScreen/SignUpScreen.tsx
+++ customer/src/Containers/Authentication/SignUpScreen/SignUpScreen.tsx
@@ -1,74 +1,81 @@
-import React, {useState, useEffect} from 'react';
-import {Image, View, Text, TextInput, ScrollView, Platform} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  Platform,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-import FastImage from 'react-native-fast-image';
+import FastImage from "react-native-fast-image";
 
-import Style from 'src/Style';
-import Styles from './SignUpScreenStyle';
-import Colors from 'src/Theme/Colors';
+import Style from "src/Style";
+import Styles from "./SignUpScreenStyle";
+import Colors from "src/Theme/Colors";
 
-import CustomPwdInput from 'src/Components/CustomForm/CustomPwdInput/CustomPwdInput';
-import CustomPhoneInput from 'src/Components/CustomForm/CustomPhoneInput/CustomPhoneInput';
-import FormCommonBtn from 'src/Components/Buttons/FormCommonBtn/FormCommonBtn';
+import CustomPwdInput from "src/Components/CustomForm/CustomPwdInput/CustomPwdInput";
+import CustomPhoneInput from "src/Components/CustomForm/CustomPhoneInput/CustomPhoneInput";
+import FormCommonBtn from "src/Components/Buttons/FormCommonBtn/FormCommonBtn";
 
-import {Images} from 'src/Theme';
+import { Images } from "src/Theme";
 
-import Toast from 'react-native-simple-toast';
+import Toast from "react-native-simple-toast";
 
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
-import {store} from 'src/Store';
-import ImagePicker from 'react-native-image-picker';
-import ImageResizer from 'react-native-image-resizer';
-import axios from 'axios';
+import { store } from "src/Store";
+import ImagePicker from "react-native-image-picker";
+import ImageResizer from "react-native-image-resizer";
+import axios from "axios";
 
-import {baseUrl, appVersion, avatarSize} from 'src/config';
-import {RESULTS} from 'react-native-permissions';
+import { baseUrl, appVersion, avatarSize } from "src/config";
+import { RESULTS } from "react-native-permissions";
 
-import {checkCamLibPermission} from 'src/Permissions';
-import EvilIconsIcon from 'react-native-vector-icons/EvilIcons';
+import { checkCamLibPermission } from "src/Permissions";
+import EvilIconsIcon from "react-native-vector-icons/EvilIcons";
 
 export default function SignUpScreen(props) {
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [otp, setOtp] = useState('');
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
   const [init, setInit] = useState(0);
   const [sentOtp, setSentOtp] = useState(false);
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [photo, setPhoto] = useState('');
-  const [localPhoto, setLocalPhoto] = useState('');
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [localPhoto, setLocalPhoto] = useState("");
 
   const sendOTP = async () => {
     await axios
-      .post(baseUrl + 'auth/otp', {
+      .post(baseUrl + "auth/otp", {
         phone,
       })
-      .then(response => {
+      .then((response) => {
         if (response.data.success) {
           setSentOtp(true);
           Toast.show(response.data.msg); //check your inbox
-          console.log('success', response.data.msg);
+          console.log("success", response.data.msg);
         } else {
           Toast.show(response.data.msg);
           setInit(init + 1);
-          console.log('failed', response.data.msg);
+          console.log("failed", response.data.msg);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
 
   const handlePhoto = async () => {
-    if (Platform.OS === 'android') {
+    if (Platform.OS === "android") {
       const ret = await checkCamLibPermission();
-      console.log('111111111111111', ret);
+      console.log("111111111111111", ret);
       if (!ret) return;
     }
 
-    ImagePicker.showImagePicker(response => {
+    ImagePicker.showImagePicker((response) => {
       if (response.didCancel) {
       } else if (response.error) {
       } else if (response.customButton) {
@@ -77,26 +84,26 @@ export default function SignUpScreen(props) {
           response.uri,
           avatarSize,
           avatarSize,
-          'JPEG',
+          "JPEG",
           100,
           0,
         )
-          .then(({uri, path, name, size}) => {
-            console.log('uri', uri, 'path', path, 'name', name, 'size', size);
+          .then(({ uri, path, name, size }) => {
+            console.log("uri", uri, "path", path, "name", name, "size", size);
 
             setLocalPhoto(uri);
             let formData = new FormData();
             const file = {
               uri,
               name,
-              type: 'image/jpeg',
+              type: "image/jpeg",
             };
-            formData.append('file', file);
+            formData.append("file", file);
 
             changePhoto(formData);
           })
-          .catch(err => {
-            console.log('resize error... ... ...', err);
+          .catch((err) => {
+            console.log("resize error... ... ...", err);
           });
       }
     });
@@ -104,71 +111,72 @@ export default function SignUpScreen(props) {
 
   async function changePhoto(formData) {
     await axios
-      .post(baseUrl + 'upload/file', formData)
-      .then(response => {
+      .post(baseUrl + "upload/file", formData)
+      .then((response) => {
         console.log(response.data.file.path);
         setPhoto(response.data.file.path);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        Toast.show('error!');
+        Toast.show("error!");
       });
   }
 
   async function handleSubmit() {
-    if (email === '' || phone === '' || photo === '' || password === '') {
-      Toast.show('input email!');
+    if (email === "" || phone === "" || photo === "" || password === "") {
+      Toast.show("input email!");
       return;
     }
 
     if (password !== confirmPassword) {
-      Toast.show('input the same passwords');
+      Toast.show("input the same passwords");
       return;
     }
 
     console.log(phone, password);
 
     axios
-      .post(baseUrl + 'auth/signup', {
+      .post(baseUrl + "auth/signup", {
         email,
         phone,
         photo,
         password,
         otp,
       })
-      .then(function(response2) {
+      .then(function (response2) {
         if (response2.data.success) {
           Toast.show(response2.data.msg);
 
-          props.navigation.navigate('SignIn');
+          props.navigation.navigate("SignIn");
         } else {
           Toast.show(response2.data.msg);
         }
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }
 
   return (
     <ScrollView>
-      <View style={{flexDirection: 'column'}}>
+      <View style={{ flexDirection: "column" }}>
         <TouchableOpacity
           onPress={handlePhoto}
           style={{
             marginRight: 15,
             // resizeMode: 'cover',
             borderRadius: 30,
-          }}>
-          <View style={{flex: 1, alignItems: 'center', marginTop: 25}}>
+          }}
+        >
+          <View style={{ flex: 1, alignItems: "center", marginTop: 25 }}>
             {localPhoto.length === 0 && (
-              <EvilIconsIcon name="user" style={{fontSize: 120}} />
+              <EvilIconsIcon name="user" style={{ fontSize: 120 }} />
             )}
             {localPhoto.length > 0 && (
               <FastImage
-                style={{width: 120, height: 120, borderRadius: 60}}
-                source={{uri: 'file://' + localPhoto}}
-                resizeMode={FastImage.resizeMode.contain}
+                style={{ width: 120, height: 120, borderRadius: 60 }}
+                source={{ uri: "file://" + localPhoto }}
+                resizeMode={FastImage.resizeMode.cover}
               />
             )}
           </View>
@@ -177,44 +185,46 @@ export default function SignUpScreen(props) {
         <View style={Styles.SignFormContainer}>
           <TextInput
             style={Style.CustomTextInput}
-            placeholder={'Email'}
-            onChangeText={value => {
+            placeholder={"Email"}
+            onChangeText={(value) => {
               setEmail(value);
             }}
           />
           <TextInput
             style={Style.CustomTextInput}
-            placeholder={'Phone number'}
-            onChangeText={value => {
+            placeholder={"Phone number"}
+            onChangeText={(value) => {
               setPhone(value);
             }}
           />
 
           <CustomPwdInput
-            CustomPwdPlaceholder={'Password'}
-            proc={value => {
+            CustomPwdPlaceholder={"Password"}
+            proc={(value) => {
               setPassword(value);
             }}
           />
 
           <CustomPwdInput
-            CustomPwdPlaceholder={'Confirm password'}
-            proc={value => {
+            CustomPwdPlaceholder={"Confirm password"}
+            proc={(value) => {
               setConfirmPassword(value);
             }}
           />
 
-          <FormCommonBtn CustomBtnTitle={'Create'} proc={handleSubmit} />
+          <FormCommonBtn CustomBtnTitle={"Create"} proc={handleSubmit} />
 
-          <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-            <Text>Already have an account? </Text>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <Text>Already have an account?</Text>
             <TouchableOpacity
-              onPress={() => props.navigation.navigate('SignIn')}>
+              onPress={() => props.navigation.navigate("SignIn")}
+            >
               <Text
                 style={{
                   color: Colors.primary,
-                  textDecorationLine: 'underline',
-                }}>
+                  textDecorationLine: "underline",
+                }}
+              >
                 Login
               </Text>
             </TouchableOpacity>
--- customer/src/Containers/Friend/FriendList/FriendList.tsx
+++ customer/src/Containers/Friend/FriendList/FriendList.tsx
@@ -1,50 +1,56 @@
-import React, {useState, useEffect, useRef, useContext} from 'react';
-import {View, Text, TouchableOpacity, FlatList, ScrollView} from 'react-native';
-import FastImage from 'react-native-fast-image';
-import {Images} from 'src/Theme';
-import CatListBtn from 'src/Components/Buttons/CatListBtn/CatListBtn';
-import Styles from './FriendListStyle';
-import Style from 'src/Style';
-import Colors from 'src/Theme/Colors';
-import Header from 'src/Components/Header/Header';
-import ProductCard from 'src/Components/Card/Product/ProductCard';
-import {tagJson} from 'src/config';
-import {baseUrl} from 'src/config';
-import {store} from 'src/Store';
+import React, { useState, useEffect, useRef, useContext } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+} from "react-native";
+import FastImage from "react-native-fast-image";
+import { Images } from "src/Theme";
+import CatListBtn from "src/Components/Buttons/CatListBtn/CatListBtn";
+import Styles from "./FriendListStyle";
+import Style from "src/Style";
+import Colors from "src/Theme/Colors";
+import Header from "src/Components/Header/Header";
+import ProductCard from "src/Components/Card/Product/ProductCard";
+import { tagJson } from "src/config";
+import { baseUrl } from "src/config";
+import { store } from "src/Store";
 
-import SearchBox from './SearchBox';
-import Toast from 'react-native-simple-toast';
-import EvilIconsIcon from 'react-native-vector-icons/EvilIcons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import SearchBox from "./SearchBox";
+import Toast from "react-native-simple-toast";
+import EvilIconsIcon from "react-native-vector-icons/EvilIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const axios = require('axios');
+const axios = require("axios");
 
 export default function FriendList(props) {
   const [state, dispatch] = useContext(store);
 
-  const [tmp, setTmp] = useState('');
-  const [key, setKey] = useState('');
+  const [tmp, setTmp] = useState("");
+  const [key, setKey] = useState("");
 
   const handleSearch = () => {
     setKey(tmp);
   };
 
-  const handleAccept = item => {
+  const handleAccept = (item) => {
     axios
-      .post(baseUrl + 'auth/accept', {
+      .post(baseUrl + "auth/accept", {
         item,
         user: state.user._id,
       })
-      .then(function(response2) {
+      .then(function (response2) {
         Toast.show(response2.data.msg);
 
         dispatch({
-          type: 'setUser',
+          type: "setUser",
           payload: response2.data.item,
         });
       })
-      .catch(function(error) {
-        Toast.show('error');
+      .catch(function (error) {
+        Toast.show("error");
         console.log(JSON.stringify(error));
       });
   };
@@ -54,90 +60,97 @@ export default function FriendList(props) {
   });
 
   return (
-    <ScrollView style={{backgroundColor: 'white'}}>
+    <ScrollView style={{ backgroundColor: "white" }}>
       <View style={Styles.CategoryListContainer}>
         <View
           style={{
             paddingLeft: 10,
             paddingRight: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-          }}>
-          <View style={{flex: 8}}>
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <View style={{ flex: 8 }}>
             <SearchBox inputProc={setTmp} handleSearch={handleSearch} />
           </View>
-          <View style={{flex: 1}}>
+          <View style={{ flex: 1 }}>
             <TouchableOpacity
               onPress={() => {
-                props.navigation.navigate('AddFriend');
-              }}>
-              <FontAwesome name="user-plus" style={{fontSize: 30}} />
+                props.navigation.navigate("AddFriend");
+              }}
+            >
+              <FontAwesome name="user-plus" style={{ fontSize: 30 }} />
             </TouchableOpacity>
           </View>
         </View>
       </View>
-      <View style={{padding: 10}}>
+      <View style={{ padding: 10 }}>
         {state.user.friends.map(
           (item, i) =>
             item.user.email.indexOf(tmp) > -1 && (
               <View
                 key={i}
                 style={{
-                  flexDirection: 'row',
+                  flexDirection: "row",
                   flex: 6,
-                  justifyContent: 'center',
-                  alignItems: 'center',
+                  justifyContent: "center",
+                  alignItems: "center",
                   padding: 10,
                   borderBottomWidth: 1,
                   borderBottomColor: Colors.border,
-                }}>
+                }}
+              >
                 <View flex={1}>
                   {!state.user.photo && (
-                    <EvilIconsIcon name="user" style={{fontSize: 50}} />
+                    <EvilIconsIcon name="user" style={{ fontSize: 50 }} />
                   )}
                   {state.user.photo && (
                     <FastImage
-                      style={{width: 50, height: 50, borderRadius: 25}}
+                      style={{ width: 50, height: 50, borderRadius: 25 }}
                       source={{
-                        uri:
-                          baseUrl + 'download/photo?path=' + state.user.photo,
+                        uri: baseUrl + "download/photo?path=" +
+                          state.user.photo,
                       }}
-                      resizeMode={FastImage.resizeMode.contain}
+                      resizeMode={FastImage.resizeMode.cover}
                     />
                   )}
                 </View>
                 <View flex={4}>
-                  <View style={{alignItems: 'center'}}>
-                    <Text style={{fontSize: 20}}>{item.user.email}</Text>
+                  <View style={{ alignItems: "center" }}>
+                    <Text style={{ fontSize: 20 }}>{item.user.email}</Text>
                   </View>
 
                   {!item.state && (
                     <View
                       style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-around',
+                        flexDirection: "row",
+                        justifyContent: "space-around",
                         paddingTop: 5,
-                      }}>
+                      }}
+                    >
                       <View />
                       <TouchableOpacity onPress={() => handleAccept(item)}>
                         <Text
                           style={{
                             color: Colors.primary,
                             fontSize: 18,
-                          }}>
+                          }}
+                        >
                           accept
                         </Text>
                       </TouchableOpacity>
                       <TouchableOpacity
                         onPress={() => {
-                          Toast.show('Rejected!');
-                        }}>
+                          Toast.show("Rejected!");
+                        }}
+                      >
                         <Text
                           style={{
                             color: Colors.primary,
                             fontSize: 18,
-                          }}>
+                          }}
+                        >
                           reject
                         </Text>
                       </TouchableOpacity>
