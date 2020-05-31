b3c407028526bf91fa3bd12d0aebb0b81f1a9ff0
shinystar <creditcoder@hotmail.com>
2020-05-31 16:17:52 -0400

    add ar empty screen

--- customer/src/App.tsx
+++ customer/src/App.tsx
@@ -1,47 +1,49 @@
-import 'react-native-gesture-handler';
-import React, {useState, useEffect, useContext} from 'react';
+import "react-native-gesture-handler";
+import React, { useState, useEffect, useContext } from "react";
 
-import {Image, Text, View} from 'react-native';
+import { Image, Text, View } from "react-native";
 
-import i18next from 'i18next';
-import {initReactI18next, useTranslation} from 'react-i18next';
+import i18next from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
-} from '@react-navigation/drawer';
+} from "@react-navigation/drawer";
 
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-import SignInScreen from 'src/Containers/Authentication/SignInScreen';
-import SignUpScreen from 'src/Containers/Authentication/SignUpScreen/SignUpScreen';
-import ForgotPwdScreen from 'src/Containers/Authentication/ForgotPwdScreen/ForgotPwdScreen';
+import SignInScreen from "src/Containers/Authentication/SignInScreen";
+import SignUpScreen from "src/Containers/Authentication/SignUpScreen/SignUpScreen";
+import ForgotPwdScreen from "src/Containers/Authentication/ForgotPwdScreen/ForgotPwdScreen";
 
-import FriendList from 'src/Containers/Friend/FriendList/FriendList';
-import AddFriend from 'src/Containers/Friend/AddFriend/AddFriend';
+import FriendList from "src/Containers/Friend/FriendList/FriendList";
+import AddFriend from "src/Containers/Friend/AddFriend/AddFriend";
 
-import Map from 'src/Containers/Map/Map';
-import AR from 'src/Containers/AR/AR';
+import Map from "src/Containers/Map/Map";
+import AR from "src/Containers/AR/AR";
 
-import {StateProvider} from 'src/Store';
+import { StateProvider } from "src/Store";
 
-import Style from 'src/Style';
-import Colors from './Theme/Colors.ts';
+import Style from "src/Style";
+import Colors from "./Theme/Colors.ts";
 
-import BgTracking from 'src/BgTracking';
+import BgTracking from "src/BgTracking";
 
-import {en, ar} from 'src/trans';
+import { en, ar } from "src/trans";
 
 const languageDetector = {
-  type: 'languageDetector',
+  type: "languageDetector",
   async: true,
-  detect: cb => cb('ar'),
+  detect: (cb) => cb("ar"),
   init: () => {},
   cacheUserLanguage: () => {},
 };
@@ -50,7 +52,7 @@ i18next
   .use(languageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ar',
+    fallbackLng: "ar",
     debug: true,
     resources: {
       en: {
@@ -72,17 +74,17 @@ const AuthStack = () => {
       <Stack.Screen
         name="SignIn"
         component={SignInScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="SignUp"
         component={SignUpScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="ForgotPwd"
         component={ForgotPwdScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
@@ -94,22 +96,33 @@ const FriendStack = () => {
       <Stack.Screen
         name="FriendList"
         component={FriendList}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="AddFriend"
         component={AddFriend}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
 };
 
-const NavStack = () => {
+const MapStack = () => {
   return (
     <Stack.Navigator initialRouteName="Map">
-      <Stack.Screen name="Map" component={Map} options={{headerShown: false}} />
-      <Stack.Screen name="AR" component={AR} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Map"
+        component={Map}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ARStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="AR">
+      <Stack.Screen name="AR" component={AR} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
@@ -118,13 +131,15 @@ const MainStack = () => {
   return (
     <Tab.Navigator
       initialRouteName="Friend"
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          if (route.name === 'Friend') {
-            iconName = 'ios-contacts';
-          } else if (route.name === 'Map') {
-            iconName = 'ios-pin';
+          if (route.name === "Friend") {
+            iconName = "ios-contacts";
+          } else if (route.name === "Map") {
+            iconName = "ios-pin";
+          } else if (route.name === "AR") {
+            iconName = "ios-navigate";
           }
           color = focused ? Colors.primary : Colors.black;
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -133,18 +148,24 @@ const MainStack = () => {
       })}
       tabBarOptions={{
         activeTintColor: Colors.primary,
-        inactiveTintColor: 'gray',
+        inactiveTintColor: "gray",
         showLabel: false,
-      }}>
+      }}
+    >
       <Tab.Screen
         name="Friend"
         component={FriendStack}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Tab.Screen
         name="Map"
-        component={NavStack}
-        options={{headerShown: false}}
+        component={MapStack}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="AR"
+        component={ARStack}
+        options={{ headerShown: false }}
       />
     </Tab.Navigator>
   );
@@ -161,16 +182,17 @@ export default function App() {
       <SafeAreaProvider>
         <NavigationContainer>
           <Stack.Navigator
-            drawerContent={props => <CustomDrawerContent {...props} />}>
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+          >
             <Stack.Screen
               name="Auth"
               component={AuthStack}
-              options={{headerShown: false}}
+              options={{ headerShown: false }}
             />
             <Stack.Screen
               name="Main"
               component={MainStack}
-              options={{headerShown: false}}
+              options={{ headerShown: false }}
             />
           </Stack.Navigator>
         </NavigationContainer>
--- customer/src/Containers/AR/AR.tsx
+++ customer/src/Containers/AR/AR.tsx
@@ -22,7 +22,7 @@ import Toast from "react-native-simple-toast";
 import EvilIconsIcon from "react-native-vector-icons/EvilIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-const axios = require("axios");
+import axios from "axios";
 
 export default function AR(props) {
   return (
