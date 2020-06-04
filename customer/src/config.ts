2365e8eae065b2ac7fafaadad2e75e78b16e5508
Pretty Dev <53688893+prettydev@users.noreply.github.com>
2020-06-03 22:22:04 -0400

    Update config.ts

--- customer/src/config.ts
+++ customer/src/config.ts
@@ -4,7 +4,7 @@ export const appVersion = '0.1';
 // export const baseUrl = 'http://10.0.2.2:8000/';
 export const baseUrl = 'http://51.38.88.155:8000/';
 
-export const apiKey = 'AIzaSyDOo1Y_JbCuuhs39Wt3i3iEyLgZXnqBkWo';
+export const apiKey = 'YOUR_GOOGLE_API_KEY';
 
 export const initialLocation = {
   latitude: 25.3592,
